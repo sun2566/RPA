3acd6b6fef24408e0e0091d7be8c5f5882e5006e
정승우 <downersw@gmail.com>
2024-10-17 16:32:00 +0900

    6주차 실습파일

---
+++ 6주차/fapi1.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/item")
+def read_item(item_id: int, name: str = None, age: int = 0):
+    return{"item_id": item_id, "name": name, "age": age}
+
+from fastapi import Form
+@app.post("/user")
+async def read_user_form(name: str = Form(...), studentcode: str = Form(...), major: str = Form(...)):
+    return {"mag": f"{major} {name}님 ({studentcode}) 반갑습니다."}
+
+from fastapi.staticfiles import StaticFiles
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host='127.0.0.1', port=8001, log_level="info")
